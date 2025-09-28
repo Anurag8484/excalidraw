95444c93ab81ad93534844f034f30e438b094529
Turbobot <turbobot@vercel.com>
2025-09-28 09:29:12 +0530

    feat(create-turbo): apply official-starter transform

--- apps/http-backend/src/routes/room/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { gate } from "../../middleware";
-
-const roomRouter = Router();
-
-
-roomRouter.post("/create-room", gate, async (req, res) => {});
--- packages/backend-common/src/index.ts
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "thisisthesecondhardestsecret";
-export const PORT = process.env.PORT || 3001;
