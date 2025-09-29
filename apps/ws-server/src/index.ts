e3bc0981e901ada189fc32e4fb965ff7b56d06c2
anurag8484 <anuragpoonia.ap@gmail.com>
2025-09-29 14:27:49 +0530

    complete chat backend

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,27 +1,36 @@
 import { WebSocketServer } from "ws";
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import { JWT_SECRET } from "@repo/backend-common/config"
-const wss = new WebSocketServer({port: 8080});
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws, request){
-    const url = request.url;
-    if (!url){
-        return;
-    };
-
-    const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token') || "";
+function checkUser(token: string): string | null {
+  try {
     const decoded = jwt.verify(token, JWT_SECRET);
+    if (!decoded || !(decoded as JwtPayload).userId) {
+      return null;
+    }
+    return (decoded as JwtPayload).id;
+  } catch (error) {
+    return null;
+  }
+}
 
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
-    };
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
 
-    ws.on('message', function message(data){
-        ws.send('pong')
-    });
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const userId = checkUser(token);
 
+  if (userId == null) {
+    ws.close();
+    return;
+  }
 
-
-})
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
+});
